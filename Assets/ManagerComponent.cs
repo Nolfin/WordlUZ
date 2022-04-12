1ea156e9f931390cdb1099f5bd13439d2a9d5359
DESKTOP-OPM9L6B\Daniel <nolfin@o2.pl>
2022-04-12 11:26:05 +0200

    Implemented Polish letters and return functions.

--- Assets/ManagerComponent.cs
+++ Assets/ManagerComponent.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +7,54 @@ public class ManagerComponent : MonoBehaviour
     // Update is called once per frame
     void OnGUI()
     {
+        int line = 1;
+        int column = 1;
         Event e = Event.current;
-        if (e.type==EventType.KeyDown && e.keyCode.ToString().Length == 1 && char.IsLetter(e.keyCode.ToString()[0]))
+        if(Input.GetKey(KeyCode.RightAlt && e.keyCode.ToString().Length == 1 && char.IsLetter(e.keyCode.ToString()[0]))
+            {
+            switch (e.keyCode)
+                {
+                case (KeyCode.E):
+                    FillIn("Ę");
+                    break;
+                case (KeyCode.O):
+                    FillIn("Ó");
+                    break;
+                case (KeyCode.L):
+                    FillIn("Ł");
+                    break;
+                case (KeyCode.S):
+                    FillIn("Ś");
+                    break;
+                case (KeyCode.A):
+                    FillIn("Ą");
+                    break;
+                case (KeyCode.Z):
+                    FillIn("Ż");
+                    break;
+                case (KeyCode.X):
+                    FillIn("Ź");
+                    break;
+                case (KeyCode.N):
+                    FillIn("Ń");
+                    break;
+                }
+            }
+        else if (e.type==EventType.KeyDown && e.keyCode.ToString().Length == 1 && char.IsLetter(e.keyCode.ToString()[0]))
         {
-            GameObject.Find("Text[1][1]");
+            FillIn(e.keyCode.ToString());
         }
+        if (Input.GetKeyDown(KeyCode.Return) && column==6)
+        {
+            column = 1;
+            line++;
+        }
+    }
+
+    public void FillIn (string c)
+    {
+        Text text = GameObject.Find("Text[" + line + "][" + column + "]").GetComponent<Text>();
+        text.text = c;
+        column++;
     }
 }
