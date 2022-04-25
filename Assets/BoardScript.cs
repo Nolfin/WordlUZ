252e2141240c04e960597bd31448630a4b03eb04
Qazen <93227765+Qazen@users.noreply.github.com>
2022-04-25 22:49:51 +0200

    Added game background and basic animations

--- Assets/BoardScript.cs
+++ Assets/BoardScript.cs
@@ -6,12 +6,16 @@ public class BoardScript : MonoBehaviour
 {
     public GameObject cubiclePrefab;
     GameObject[,] cubicles;
+    public GameObject winningAnimationObject;
+    public GameObject losingAnimationObject;
 
     // Start is called before the first frame update
     void Start()
     {
         cubicles = new GameObject[6, 5];
         GenerateCubicles();
+
+        Invoke("playLosingAnimation", 3.0f); //just to showcase it working
     }
 
     // Update is called once per frame
@@ -28,8 +32,18 @@ public class BoardScript : MonoBehaviour
             {
                 cubicles[i,j] = (GameObject)Instantiate(cubiclePrefab);
                 cubicles[i, j].name = "Text[" + (5-i) + "][" + j + "]";
-                cubicles[i, j].transform.position = new Vector3(j * 1.1f - 2.75f, i * 1.1f - 3.3f, 0.0f);
+                cubicles[i, j].transform.position = new Vector3(j * 1.1f - 2.75f + 0.55f, i * 1.1f - 3.3f, 0.0f);
             }
-        }    
+        }
+    }
+
+    void playWinningAnimation()
+    {
+        winningAnimationObject.GetComponent<Animator>().Play("sliderAnim");
+    }
+
+    void playLosingAnimation()
+    {
+        losingAnimationObject.GetComponent<Animator>().Play("sliderAnim");
     }
 }
