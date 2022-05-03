50f4244fc1458262331b89542546b56f9671f5b9
DESKTOP-OPM9L6B\Daniel <nolfin@o2.pl>
2022-05-03 17:20:43 +0200

    Added Game Mode - Liars, Added 3 new buttons for game modes, Added button functionality

--- Assets/ManagerComponent.cs
+++ Assets/ManagerComponent.cs
@@ -1,11 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class ManagerComponent : MonoBehaviour
 {
+
+    private short activeMode = 0;
     private int column;
     private readonly string[] inputWord = new string[5];
     private int line;
@@ -14,13 +19,14 @@ public class ManagerComponent : MonoBehaviour
     private string[] theWordArray = new string[5];
 
     // Update is called once per frame
+    
     private void OnGUI()
     {
         var e = Event.current;
         if (Input.GetKeyDown(KeyCode.Return) && column == 5 && e.type == EventType.KeyDown)
         {
-            column = 0;
             if (!IsCorrectWord()) return;
+            column = 0;
             if (line == 0)
             {
                 theWord = RollAWord();
@@ -31,6 +37,7 @@ public class ManagerComponent : MonoBehaviour
                 }
             }
             CheckForSimilarities();
+            if (activeMode == 1) ChangeRandomLetterColour();
             line++;
         }
 
@@ -78,16 +85,19 @@ public class ManagerComponent : MonoBehaviour
         else if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1 &&
                  char.IsLetter(e.keyCode.ToString()[0])) FillIn(e.keyCode.ToString());
 
-        void FillIn(string c)
-        {
-            var text = GameObject.Find("Text[" + line + "][" + column + "]").transform.GetChild(0).transform.GetChild(1)
-                .GetComponent<TextMeshPro>();
-            text.text = c;
-            inputWord[column] = c;
-            column++;
-        }
-    }
+        EventSystem.current.SetSelectedGameObject(null);
 
+
+
+    }
+    void FillIn(string c)
+    {
+        var text = GameObject.Find("Text[" + line + "][" + column + "]").transform.GetChild(0).transform.GetChild(1)
+            .GetComponent<TextMeshPro>();
+        text.text = c;
+        inputWord[column] = c;
+        column++;
+    }
     public string RollAWord()
     {
         string result;
@@ -160,5 +170,98 @@ public class ManagerComponent : MonoBehaviour
             
             
         }
+    }
+
+    public void ChangeModeTo1()
+    {
+        for (double i = 0; i < 30; i++)
+        {
+            GameObject.Find("Text[" + Math.Floor(i/5) + "][" + i%5 + "]").transform.GetChild(0).transform.GetChild(1)
+                .GetComponent<TextMeshPro>().SetText(" ");
+            GameObject.Find("Text[" + Math.Floor(i / 5) + "][" + i % 5 + "]").transform.GetChild(0).transform
+                .GetChild(1)
+                .GetComponent<TextMeshPro>().color = new Color(220, 220, 220, 255);
+            Debug.Log("Liers");
+        }
+
+        line = 0;
+        column = 0;
+        activeMode = 1;
+    }
+    
+    public void ChangeModeTo0()
+    {
+        for (double i = 0; i < 30; i++)
+        {
+            GameObject.Find("Text[" + Math.Floor(i/5) + "][" + i%5 + "]").transform.GetChild(0).transform.GetChild(1)
+                .GetComponent<TextMeshPro>().SetText(" ");
+            GameObject.Find("Text[" + Math.Floor(i / 5) + "][" + i % 5 + "]").transform.GetChild(0).transform
+                .GetChild(1)
+                .GetComponent<TextMeshPro>().color = new Color(220, 220, 220, 255);
+        }
+
+        line = 0;
+        column = 0;
+        activeMode = 0;
+    }
+    
+    public void ChangeModeTo2()
+    {
+        for (double i = 0; i < 30; i++)
+        {
+            GameObject.Find("Text[" + Math.Floor(i/5) + "][" + i%5 + "]").transform.GetChild(0).transform.GetChild(1)
+                .GetComponent<TextMeshPro>().SetText(" ");
+            GameObject.Find("Text[" + Math.Floor(i / 5) + "][" + i % 5 + "]").transform.GetChild(0).transform
+                .GetChild(1)
+                .GetComponent<TextMeshPro>().color = new Color(220, 220, 220, 255);
+        }
+
+        line = 0;
+        column = 0;
+        activeMode = 2;
+    }
+    public void NewGame()
+    {
+        for (double i = 0; i < 30; i++)
+        {
+            GameObject.Find("Text[" + Math.Floor(i/5) + "][" + i%5 + "]").transform.GetChild(0).transform.GetChild(1)
+                .GetComponent<TextMeshPro>().SetText(" ");
+            GameObject.Find("Text[" + Math.Floor(i / 5) + "][" + i % 5 + "]").transform.GetChild(0).transform
+                .GetChild(1)
+                .GetComponent<TextMeshPro>().color = new Color(220, 220, 220, 255);
+        }
+
+        line = 0;
+        column = 0;
+    }
+
+    public void ChangeRandomLetterColour()
+    {
+        var random = new Random();
+        var letterToLie = random.Next(0,4);
+        var colorToChange = GameObject.Find("Text[" + line + "][" + letterToLie + "]").transform.GetChild(0).transform
+            .GetChild(1)
+            .GetComponent<TextMeshPro>().color;
+        if (colorToChange == new Color(0, 255, 0, 255))
+        {
+            if(random.Next(0, 1)==0) colorToChange = new Color(220, 220, 220, 255);
+            else colorToChange = new Color(255, 255, 0, 255);
+            Debug.Log("Lied 1"+letterToLie);
+        }
+        if (colorToChange == new Color(220, 220, 220, 255))
+        {
+            if(random.Next(0, 1)==0) colorToChange = new Color(0, 255, 0, 255);
+            else colorToChange = new Color(255, 255, 0, 255);
+            Debug.Log("Lied 2"+letterToLie);
+        }
+        if (colorToChange == new Color(255, 255, 0, 255))
+        {
+            if(random.Next(0,1)==0) colorToChange = new Color(220, 220, 220, 255);
+            else colorToChange = new Color(0, 255, 0, 255);
+            Debug.Log("Lied 3"+letterToLie);
+        }
+        GameObject.Find("Text[" + line + "][" + letterToLie + "]").transform.GetChild(0).transform
+            .GetChild(1)
+            .GetComponent<TextMeshPro>().color = colorToChange;
     }
 }
