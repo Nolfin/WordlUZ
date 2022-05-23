a06c3ec06a1e95988e65da65c8bdf1d843ebc392
Qazen <93227765+Qazen@users.noreply.github.com>
2022-05-24 01:09:35 +0200

    Update BoardScript.cs
    Timer implementation attempt within board script

--- Assets/BoardScript.cs
+++ Assets/BoardScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardScript : MonoBehaviour
 {
@@ -8,21 +9,64 @@ public class BoardScript : MonoBehaviour
     GameObject[,] cubicles;
     public GameObject winningAnimationObject;
     public GameObject losingAnimationObject;
+    private float timeRemaining;
+    private bool hasRanOutOfTime;
+    private bool hasWon;
+    private bool hasLost;
+    private Text text;
 
     // Start is called before the first frame update
     void Start()
     {
         cubicles = new GameObject[6, 5];
         GenerateCubicles();
+        timeRemaining = 120;
+        hasRanOutOfTime = false;
+        hasLost = false;
+        hasWon = false;
 
-        Invoke("playLosingAnimation", 3.0f); //just to showcase it working
+        //Invoke("playLosingAnimation", 3.0f); //just to showcase it working
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeRemaining > 0 && !hasWon && !hasLost)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else if (!hasRanOutOfTime)
+        {
+            hasRanOutOfTime = true;
+            Invoke("playLosingAnimation", 0.0f);
+        }
     }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 20), getRoundedTime());
+    }
+
+    private string getRoundedTime()
+    {
+        if (hasRanOutOfTime)
+        {
+            return "Out of time!";
+        }
+        else
+        {
+            if (timeRemaining <= 0)
+            {
+                return "Time: 0";
+            }
+            else
+            {
+                return "Time: " + Mathf.RoundToInt(timeRemaining).ToString();
+            }
+        }
+    }
+
+
 
     void GenerateCubicles()
     {
@@ -40,10 +84,12 @@ public class BoardScript : MonoBehaviour
     void playWinningAnimation()
     {
         winningAnimationObject.GetComponent<Animator>().Play("sliderAnim");
+        hasWon = true;
     }
 
     void playLosingAnimation()
     {
         losingAnimationObject.GetComponent<Animator>().Play("sliderAnim");
+        hasLost = true;
     }
 }
